31ed3ed6f3b77f701512b33135751167e7dd835b
Moklev Vyacheslav <slavam2605@bk.ru>
2018-05-04 00:04:26 +0300

    Fix again

--- src/Language.ml
+++ src/Language.ml
@@ -36,7 +36,7 @@ module Value =
     let rec list_init i n f = if i >= n then [] else (f i) :: (list_init (i + 1) n f) 
 
     let update_string s i x = String.init (String.length s) (fun j -> if j = i then x else s.[j])
-    let update_array  a i x = list_init   (List.length a)   (fun j -> if j = i then x else List.nth a j)
+    let update_array  a i x = list_init 0 (List.length a)   (fun j -> if j = i then x else List.nth a j)
 
   end
        
