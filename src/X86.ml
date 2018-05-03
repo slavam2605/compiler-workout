e5cd7220014b1702c662c2fe8e6d06481e0d8a19
Moklev Vyacheslav <slavam2605@bk.ru>
2018-05-04 00:09:26 +0300

    Fix again 2

--- src/X86.ml
+++ src/X86.ml
@@ -106,8 +106,10 @@ let compile env code =
 (* A set of strings *)           
 module S = Set.Make (String)
 
+let rec list_init i n f = if i >= n then [] else (f i) :: (list_init (i + 1) n f)
+
 (* Environment implementation *)
-let make_assoc l = List.combine l (List.init (List.length l) (fun x -> x))
+let make_assoc l = List.combine l (list_init 0 (List.length l) (fun x -> x))
                      
 class env =
   object (self)
